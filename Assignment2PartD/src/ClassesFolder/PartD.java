2cda7ed1e87b4482a1f5e088e34b631193a03a5c
AndreyA95 <andrey.andreev95@gmail.com>
2020-06-04 23:04:38 +1200

    Third line added for third commit.

--- Assignment2PartD/src/ClassesFolder/PartD.java
+++ Assignment2PartD/src/ClassesFolder/PartD.java
@@ -5,6 +5,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("My name is Andrey.");
 		System.out.println("My favourite colour is blue.");
+		System.out.println("My favourite form of transport is by plane.");
 	}
 
 }
