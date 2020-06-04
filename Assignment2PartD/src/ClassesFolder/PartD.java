07b4a4b2852b098e86c8ecd7a7cbf10ae5befd2a
AndreyA95 <andrey.andreev95@gmail.com>
2020-06-04 23:06:23 +1200

    Fourth and final line to commit.

--- Assignment2PartD/src/ClassesFolder/PartD.java
+++ Assignment2PartD/src/ClassesFolder/PartD.java
@@ -6,6 +6,7 @@ public class PartD {
 		System.out.println("My name is Andrey.");
 		System.out.println("My favourite colour is blue.");
 		System.out.println("My favourite form of transport is by plane.");
+		System.out.println("The best day of the week is Sunday because it's my Friday.");
 	}
 
 }
